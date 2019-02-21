640c772c3134f707242dc85500716c624bdf115b
Priya <priyaraskar@gmail.com>
2019-02-20 16:11:01 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,12 @@
 $(function() {
   $("#divBox").on('mouseover mouseleave', colored);
-  $("#divBox").on("click",function(){
-		$("#divBox").off("mouseover mouseleave", colored);// removes the set events mouseover and mouseleave
-		$("#divBox").removeClass("setColor");// removes the class if its there
+  
+	$("#divBox").on("click",function(){
+		$("#divBox").off("mouseover mouseleave", colored);
+		// removes the set events mouseover and mouseleave
+		
+		$("#divBox").removeClass("setColor");
+		// removes the class if its there
 	}); 
 	
 	function colored(){
